791ddbf084652b362ba4e1fd58f3dd58cbaf8916
Ulyana Vladimir <vladimir.ulya@mail.ru>
2018-05-10 19:35:47 +0300

    final

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -16,7 +16,7 @@ $this->title = 'Головна';
         <div class="row">
             <div class="col-sm-4">
                 <div class="insta-by">
-                    <a href="/site/instaurl">
+                    <a href="/instaurl">
                         <span class="insta-by-text">Пошук по url</span>
                         <img class="insta-img" decoding="auto" style sizes="293px" srcset="https://scontent-frx5-1.cdninstagram.com/vp/15d1343ad4fc0f1e72f34664349be41c/5B995135/t51.2885-15/s150x150/e35/c0.134.1080.1080/30841299_573733693008188_6748950689394720768_n.jpg 150w,https://scontent-frx5-1.cdninstagram.com/vp/3310992dfc5dfc4c4ff27f50a5f98c87/5B7A965E/t51.2885-15/s240x240/e35/c0.134.1080.1080/30841299_573733693008188_6748950689394720768_n.jpg 240w,https://scontent-frx5-1.cdninstagram.com/vp/960426b26ad3bc89889b8a72412b59ff/5B99EF96/t51.2885-15/s320x320/e35/c0.134.1080.1080/30841299_573733693008188_6748950689394720768_n.jpg 320w,https://scontent-frx5-1.cdninstagram.com/vp/0e7962c56270bd4d870c3f74fd356bde/5B5F777C/t51.2885-15/s480x480/e35/c0.134.1080.1080/30841299_573733693008188_6748950689394720768_n.jpg 480w,https://scontent-frx5-1.cdninstagram.com/vp/3b827db0b336c3a5c60434a25d9a4040/5B7176DB/t51.2885-15/s640x640/sh0.08/e35/c0.134.1080.1080/30841299_573733693008188_6748950689394720768_n.jpg 640w" src="https://scontent-frx5-1.cdninstagram.com/vp/3b827db0b336c3a5c60434a25d9a4040/5B7176DB/t51.2885-15/s640x640/sh0.08/e35/c0.134.1080.1080/30841299_573733693008188_6748950689394720768_n.jpg"/>
                     </a>
@@ -24,7 +24,7 @@ $this->title = 'Головна';
             </div>
             <div class="col-sm-4">
                 <div class="insta-by">
-                    <a href="/site/insta">
+                    <a href="/insta">
                         <span class="insta-by-text">Пошук по імені</span>
                         <img class="insta-img" decoding="auto" style sizes="293px" srcset="https://scontent-frx5-1.cdninstagram.com/vp/a8b77721b96653c1384ddcd1e1d4e5f6/5B5F465E/t51.2885-15/s150x150/e35/c0.100.800.800/26342865_918246368340938_3111686956795297792_n.jpg 150w,https://scontent-frx5-1.cdninstagram.com/vp/0d82a112a6667426e1682ca9f8425b75/5B66B9C8/t51.2885-15/s240x240/e35/c0.100.800.800/26342865_918246368340938_3111686956795297792_n.jpg 240w,https://scontent-frx5-1.cdninstagram.com/vp/1ba32547a5abb6557b91e3969a1d893e/5B57DE20/t51.2885-15/s320x320/e35/c0.100.800.800/26342865_918246368340938_3111686956795297792_n.jpg 320w,https://scontent-frx5-1.cdninstagram.com/vp/0ec3b5c095ee07d439ba2a6a3c821d80/5B603309/t51.2885-15/s480x480/e35/c0.100.800.800/26342865_918246368340938_3111686956795297792_n.jpg 480w,https://scontent-frx5-1.cdninstagram.com/vp/66056cf173fc58ed73af1dd3fa2f755c/5B5B911D/t51.2885-15/s640x640/sh0.08/e35/c0.100.800.800/26342865_918246368340938_3111686956795297792_n.jpg 640w" src="https://scontent-frx5-1.cdninstagram.com/vp/66056cf173fc58ed73af1dd3fa2f755c/5B5B911D/t51.2885-15/s640x640/sh0.08/e35/c0.100.800.800/26342865_918246368340938_3111686956795297792_n.jpg"/>
                     </a>
@@ -32,7 +32,7 @@ $this->title = 'Головна';
             </div>
             <div class="col-sm-4">
                 <div class="insta-by">
-                    <a href="/site/instatag">
+                    <a href="/instatag">
                         <span class="insta-by-text">Пошук по тегу</span>
                         <img class="insta-img" decoding="auto" style sizes="293px" srcset="https://scontent-frx5-1.cdninstagram.com/vp/6caa4873db0d247b0ea9aaf210feb264/5B60E080/t51.2885-15/s150x150/e35/c0.100.800.800/26274132_1804831146480915_258403511908171776_n.jpg 150w,https://scontent-frx5-1.cdninstagram.com/vp/3bdf2f88c599dd57760d8ba569ac5753/5B602616/t51.2885-15/s240x240/e35/c0.100.800.800/26274132_1804831146480915_258403511908171776_n.jpg 240w,https://scontent-frx5-1.cdninstagram.com/vp/5c98df91a1ec201334ddb21931cb8b79/5B9B3FFE/t51.2885-15/s320x320/e35/c0.100.800.800/26274132_1804831146480915_258403511908171776_n.jpg 320w,https://scontent-frx5-1.cdninstagram.com/vp/7153666be3dded1f8e499017a8690235/5B697FD7/t51.2885-15/s480x480/e35/c0.100.800.800/26274132_1804831146480915_258403511908171776_n.jpg 480w,https://scontent-frx5-1.cdninstagram.com/vp/5285294c637878cc8a8e6ade15c5419a/5B5C3FC3/t51.2885-15/s640x640/sh0.08/e35/c0.100.800.800/26274132_1804831146480915_258403511908171776_n.jpg 640w" src="https://scontent-frx5-1.cdninstagram.com/vp/5285294c637878cc8a8e6ade15c5419a/5B5C3FC3/t51.2885-15/s640x640/sh0.08/e35/c0.100.800.800/26274132_1804831146480915_258403511908171776_n.jpg"/>
                     </a>
